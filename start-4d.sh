380e7cd0462004dd86eafa05bacb9356784210ed
bamousse <53902749+bamousse@users.noreply.github.com>
2020-05-29 20:05:08 +0100

    Delete start-4d.sh

--- start-4d.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-server=/opt/4d-server/bin/4d-server
-project=`find /opt/app/ -name "*.4DProject" | head -n 1`
-data=`find /opt/app/ -name "*.4DD" | head -n 1`
-[[ ! -z "$data" ]] && data_param="-d "$data || data_param="--create-data"
-[[ ! -z "$project" ]] &&  $server -s $project $data_param || echo "Could not find a 4D Project to be started"
